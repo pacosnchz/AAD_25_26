595c735efade8f4b5465b6a6e8a42b89905ad77a
paco <pacosnchz@icloud.com>
2025-10-02 12:31:56 +0200

    por fin

--- src/main/java/com/paco/aad/AadApplication.java
+++ src/main/java/com/paco/aad/AadApplication.java
@@ -1,13 +1,34 @@
 package com.paco.aad;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
 
 @SpringBootApplication
-public class AadApplication {
+@Slf4j
+public class AadApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AadApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+
+		log.info("Introduce una  ruta:");
+
+
+		Path ruta = Path.of(scanner.nextLine());
+
+		if (Files.exists(ruta)) {
+			log.info("Bienvenido a la ruta: " + ruta.toAbsolutePath());
+		} else {
+			log.info("La ruta no existe");
+		}
+	}
 }
